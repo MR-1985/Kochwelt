92b3594e725bd7488cea19177acf9e309c8387e7
Marco Rößler <marco_r1985@yahoo.de>
2025-07-10 15:03:26 +0200

    recept_MR.html  is shorter now; template_recept_MR.js is new

--- recept_MR.js
+++ recept_MR.js
@@ -1,42 +1,57 @@
 function toggleMenu() {
-    document.getElementById("sideMenu").classList.toggle('sideMenuHide')
-  }
-  
-const howManyNeed = [150, 0.5, 0.5, 10, 1, 1, 1, 1, 0.5];
-  
-const howManyDiv = ["howMuchGreyOne", "howMuchWhiteOne", "howMuchGreyTwo", "howMuchWhiteTwo", "howMuchGreyThree", "howMuchWhiteThree", "howMuchGreyFour", "howMuchWhiteFour", "howMuchGreyFive"];
-  
-  function calculatePortion() {
-    let portion = document.getElementById("input").value;
+  document.getElementById("sideMenu").classList.toggle('sideMenuHide')
+}
 
-      for (let i = 0; i < howManyNeed.length; i++){
-          let calculatedPortion = portion * howManyNeed[i];
-          document.getElementById(howManyDiv[i]).innerText = calculatedPortion;
-          }
-          if (portion>21) {
-            alert('Es sind nur max 21 Portionen zu errechnen!')
-            portion = 1;
-            document.getElementById('input').value = portion;
-            for (let i = 0; i < howManyDiv.length; i++) {
-              document.getElementById(howManyDiv[i]).innerText = howManyNeed[i]; 
-            }
-            return;
-          }else if (portion<1) {
-            alert('Es muss mindestens 1 Portion sein')
-            portion = 1;
-            document.getElementById('input').value = portion;
-              for (let i = 0; i < howManyDiv.length; i++) {
-                document.getElementById(howManyDiv[i]).innerText = howManyNeed[i]; 
-              }
-          return;
-      }
+const howManyNeed = [150, 0.5, 0.5, 10, 1, 1, 1, 1, 0.5];
+const ingredients = ["Hähnchenbrustfilet", "Paprika (rot oder gelb)", "Zucchini", "g Erbsen", "große Tomate(n)", "Mohrrübe(n)", "kleine Zwiebel(n)", "EL Olivenöl", "TL Kräuter der Provence (oder nach Geschmack)"];
+
+function init() {
+  document.getElementById("ingredientsContainer").innerHTML = '';
+  for (let i = 0; i < howManyNeed.length; i++) {
+    let cssClass = (i % 2 !== 0) ? 'white' : 'grey';
+    let countRef = document.getElementById("count");
+    let counter = howManyNeed[i];
+    let ingredientName = ingredients[i];
+    document.getElementById("selectOptions").innerHTML = renderSelectOptions();
+    document.getElementById("accessoriesContainer").innerHTML = renderAccessoriesContainerTemplate();
+    document.getElementById("preparationContainer").innerHTML = renderPreparationContainerTemplate();
+    document.getElementById("ingredientsContainer").innerHTML += renderStandardTemplate(cssClass, countRef, counter, ingredientName);
+  };
+}
+
+function calculatePortion() {
+  let portion = parseFloat(document.getElementById("input").value);
+  if (!checkInputValue(portion)) {
+    return;
   }
-  
-      function calculatePortionTwo(){
-        let portionTwo = document.getElementById('portionSelect').value;
-        for (let i = 0; i < howManyNeed.length; i++){
-           let calculatedPortionTwo = portionTwo * howManyNeed[i];
-           document.getElementById(howManyDiv[i]).innerText = calculatedPortionTwo;
-        }
-      }
-  
+  document.getElementById("ingredientsContainer").innerHTML = '';
+  for (let i = 0; i < howManyNeed.length; i++) {
+    let calculatedPortion = portion * howManyNeed[i];
+    let cssClass = (i % 2 !== 0) ? 'white' : 'grey';
+    let ingredientName = ingredients[i];
+    let countRef = `count-${i}`;
+    document.getElementById("ingredientsContainer").innerHTML += ingredientsTemplate(cssClass, countRef, calculatedPortion, ingredientName);
+  }
+}
+function checkInputValue(portion) {
+  if (portion > 21) {
+    alert('Es sind nur max 21 Portionen zu errechnen!')
+    document.getElementById('input').value = 1;
+    init();
+    return false;
+  } else if (portion < 1 || isNaN(portion)) {
+    alert('Es muss mindestens 1 Portion sein')
+    document.getElementById('input').value = 1;
+    init();
+    return false;
+  }
+  return true
+}
+
+// function calculatePortionTwo() {
+//   let portionTwo = document.getElementById('portionSelect').value;
+//   for (let i = 0; i < howManyNeed.length; i++) {
+//     let calculatedPortionTwo = portionTwo * howManyNeed[i];
+//     document.getElementById(howManyDiv[i]).innerText = calculatedPortionTwo;
+//   }
+// }
---
+++ template_recept_MR.js
@@ -0,0 +1,93 @@
+function renderStandardTemplate(cssClass, countRef, counter, ingredientName) {
+    return `
+    <div class="${cssClass}">
+        <div id="${countRef}">
+            ${counter}
+        </div>
+        <div class="what">
+            ${ingredientName}
+        </div>
+    </div>`
+}
+
+function ingredientsTemplate(cssClass, countRef, calculatedPortion, ingredientName) {
+    return `
+    <div class="${cssClass}">
+        <div id="${countRef}">
+            ${calculatedPortion}
+        </div>
+        <div class="what">
+            ${ingredientName}
+        </div>
+    </div>`
+}
+
+function renderSelectOptions() {
+    return `
+    <label for="input">Zutaten für &nbsp;</label>
+                            <input type="number" id="input" max="21" min="1" value="1" required>
+                            <button id="buttonPortion2" onclick="calculatePortion()">Portionen</button>
+                            <button id="buttonPortion" onclick="calculatePortionTwo()">Portionen</button>
+                            <select id="portionSelect" class="portionSelectWidth" required>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                            </select>`
+}
+
+function renderAccessoriesContainerTemplate() {
+    return `
+    <div class="accessories">
+                            <img src="./assets/icons/clock-regular.svg" alt="Uhrensymbol">
+                            <p>&nbsp;&nbsp;ca. 10 Minuten</p>
+                        </div>
+                        <div class="accessories">
+                            <img src="./assets/icons/clock-regular.svg" alt="Uhrensymbol">
+                            <p>&nbsp;&nbsp;Gesamtzeitzeit ca. 20 Minuten</p>
+                        </div>`
+}
+
+function renderPreparationContainerTemplate() {
+    return`
+    <ol>
+                            <li>Hähnchenbrust in kleine Stücke schneiden.</li><br>
+                            <li>Paprika, Zucchini, Mohrrübe und Tomaten ebenfalls in kleine Stücke schneiden.</li>
+                            <br>
+                            <li>Zwiebel schälen und in feine Würfel schneiden.</li><br>
+                            <li>In einer Pfanne das Olivenöl erhitzen und die Zwiebel darin anbraten, bis sie glasig
+                                wird.
+                            </li>
+                            <br>
+                            <li>Die Hähnchenbruststücke hinzufügen und rundherum anbraten, bis sie goldbraun sind.
+                            </li><br>
+                            <li>Die vorbereiteten Gemüse (Paprika, Zucchini, Mohrrübe, Erbsen, Tomaten) in die
+                                Pfanne geben
+                                und
+                                alles gut
+                                vermischen.</li><br>
+                            <li>Mit den Kräutern der Provence würzen und bei mittlerer Hitze etwa 5-7 Minuten
+                                weiterbraten,
+                                bis
+                                das Gemüse weich ist.</li><br>
+                            <li>Nach Belieben mit Salz und Pfeffer abschmecken.</li>
+                        </ol>`
+}
+
